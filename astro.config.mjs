4185e90e13b2c1a89ea4f2a96c190ece48a8f402
Alexsander532 <alexsanderaugusto142019@gmail.com>
2026-02-14 19:22:09 -0300

    qreweqw

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 // https://astro.build/config
 export default defineConfig({});
+
